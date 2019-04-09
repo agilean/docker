4fc5ff94bfe979c8f34342b5457e0d3a06151ce1
admin@example.com <jiangpeng@agilean.cn>
2019-04-09 20:19:29 +0800

    vue-mint

---
+++ dlm_front/readme.txt
@@ -0,0 +1,1 @@
+git clone https://github.com/Panda-Hope/vue-qqmusic
---
+++ dlm_front/utils/menus.js
@@ -0,0 +1,6 @@
+
+export default {
+	icons:[{'name':'主页','image':'../images/home_active.png'},{'name':'客服','image':'../images/kefu.png'},{'name':'门店','image':'../images/shop.png'},{'name':'购物车','image':'../images/cart.png'},{'name':'个人','image':'../images/me.png'}],
+	active_icons:[{'主页':'home_active.png'},{'客服':'kefu_active.png'},{'门店':'shop_active.png'},{'购物车':'cart_active.png'},{'个人':'me_active.png'}]
+}
+
