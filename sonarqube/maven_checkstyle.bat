0e8816cccc8ca2d10a4b70c9867d17ed16c1f2f2
admin@example.com <jiangpeng@agilean.cn>
2019-04-11 11:48:42 +0800

    Docker sonarqube

---
+++ sonarqube/maven_checkstyle.bat
@@ -0,0 +1,9 @@
+
+set MAVEN_HOME=F:\software\apache-maven-3.5.2
+set JAVA_HOME=F:\software\JDK
+
+cd F:\shengzheng\平安银行汽融科技交付物-姜鹏\sonar-plugins\sonar-checkstyle-master
+
+F:
+
+%MAVEN_HOME%\bin\mvn clean verify -DskipTests -X 
